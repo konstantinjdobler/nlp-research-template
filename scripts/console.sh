10d224fe42627e2af0ca0c80de7e8e37e606de47
Maximilian Schall <Maxscha@users.noreply.github.com>
2023-04-24 18:05:47 +0200

    Add devcontainer & development setup (#6)
    Co-authored-by: Maximilian Schall <Maximilian.Schall@mail.de>
    Co-authored-by: Konstantin Dobler <konstantin.j.dobler@gmail.com>

---
+++ scripts/console.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set e
+
+# Check if the current directory is named "scripts"
+current_directory=$(basename "$PWD")
+
+if [ "$current_directory" == "scripts" ]; then
+  echo "Error: This script should be called from the root of the project."
+  echo "Example: bash ./scripts/console.sh"
+  exit 1
+fi
+
+docker run -it \
+    --gpus='device=CHANGE_ME' \
+    --ipc host \
+    --env WANDB_API_KEY \
+    -v "source=/CHANGE_ME/.cache,target=/mamba/.cache,type=bind" \
+    -v "$(pwd)":/workspace \
+    -w /workspace \
+    DOCKER_TAG_CHANGE_ME \
+    bash
