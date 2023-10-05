36a76a435436c20e1bd95117425800c0a3339e35
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2023-10-05 17:52:04 +0200

    rm old `dlib`

--- dlib/frameworks/huggingface.py
+++
@@ -1,7 +0,0 @@
-import transformers
-
-
-class EvaluateFirstStepCallback(transformers.TrainerCallback):
-    def on_step_end(self, args, state, control, **kwargs):
-        if state.global_step == 1:
-            control.should_evaluate = True
