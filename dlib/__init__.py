0841751b9773c11a1a28d29547e97295bd730319
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2023-10-05 21:51:54 +0200

    add imports

--- dlib/__init__.py
+++ dlib/__init__.py
@@ -1,7 +1,12 @@
 # ruff: noqa: F401
 from .data.dataset import get_dataloaders
 from .frameworks.any_precision_optimizer import AnyPrecisionAdamW
-from .frameworks.fabric_lightning import fabric_main_process_first, save_checkpoint
+from .frameworks.fabric_lightning import (
+    CUDAMetricsCallback,
+    WandbCleanupDiskAndCloudSpaceCallback,
+    fabric_main_process_first,
+    save_checkpoint,
+)
 from .frameworks.model_profiling import log_model_stats_to_wandb
 from .frameworks.pytorch import get_rank, main_process_first, num_parameters, set_torch_file_sharing_strategy_to_system
 from .frameworks.speed_monitor import SpeedMonitorFabric, measure_model_flops
