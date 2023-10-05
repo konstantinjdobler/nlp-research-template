0d6dac2c84aef112b09c19baae83331a51e3a412
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2023-10-05 21:51:12 +0200

    update `TrainingArgs`

--- dlib/training_args.py
+++ dlib/training_args.py
@@ -1,3 +1,5 @@
+import multiprocessing
+import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
@@ -12,19 +14,42 @@ class TrainingArgs:
     """
 
     data_dir: Path = field(alias="-d")
-    run_name: str = field(aliase="-n")  # Run name for logging
 
-    base_unit: Literal["samples", "tokens", "optimizer-steps", "iters"] = field(default="iters")
-    "Unit of all training constants. They will be converted to optimizer_steps and iters in __post_init__."
+    model_path: str = field(alias="--model")
+    "Model identifier. This is used to construct the model architecture and load pretrained weights if not specified otherwise."
+
+    from_scratch: bool = field(default=False)
+    "Do not use pre-trained weights to intialize the model."
+
+    saved_checkpoint_path: str | None = field(default=None, alias="--checkpoint")
+    "Path to a checkpoint saved during training. Use the wandb:<wandb-run-id> syntax to load a checkpoint from W&B."
+
+    resume: bool = False
+
+    train_file: str = field(default="train.txt")
+    "Name of the training file."
+
+    val_file: str = field(default="val.txt")
+    "Name of the validation file."
+
+    tokenizer_path: str | None = field(default=None)
+    "Path to a saved tokenizer to switch the vocabulary. If None, use the model_path."
+
+    ###############################
+    ##### Training constants ######
+    ###############################
+
+    base_unit: Literal["samples", "tokens", "optimizer-steps", "iters"] = field(default="optimizer-steps")
+    "Unit of all training constants. They will be converted to optimizer_steps in __post_init__."
 
     training_goal: int = field(default=100_000)
     eval_interval: float = field(default=0.1)
     "Interval between evaluations. If < 1, use as percentage of training_goal."
 
-    eval_samples: int = field(default=100)
-    "Number of iters on the val dataset during evaluation. If -1, use full val dataset."
+    eval_samples: int = field(default=-1)
+    "Number of samples on the val dataset during evaluation. If -1, use full val dataset."
 
-    save_interval: float = field(default=0.1)
+    save_interval: int | float = field(default=0.1)
     "Interval between model checkpoints. If < 1, use as percentage of training_goal."
 
     log_interval: float = field(default=-1)
@@ -36,18 +61,14 @@ class TrainingArgs:
     lr_decay_period: int = field(default=-1)
     "If -1, decay until end of training."
 
-    seed: int = field(default=42)
-    only_val: bool = field(default=False)
-
-    pretrained_checkpoint_dir: Path = field(default="checkpoints/meta-llama/Llama-2-7b-hf")
-    resume: bool = False
-    out_dir: Path = field(default="out/")
+    ###########################
+    ##### Hyperparameters #####
+    ###########################
+    block_size: int = field(default=512)
+    "The sequence length of samples."
 
     learning_rate: float = field(default=3e-4)
     batch_size: int = field(default=128, alias="-b")
-    block_size: int | None = field(default=None)
-    "If None, load from model config."
-
     weight_decay: float = 0.1
     beta1: float = 0.9
     beta2: float = 0.95
@@ -56,6 +77,59 @@ class TrainingArgs:
 
     decay_lr: bool = True
     min_lr: float = 6e-5
+
+    #######################################
+    ## Hardware acceleration & precision ##
+    #######################################
+
+    accelerator: Literal["cuda", "cpu", "tpu", "mps"] = field(default="cuda")
+    "Hardware accelerator to use."
+    num_devices: int = field(default=1)
+    activation_checkpointing: bool = field(default=True)
+
+    distributed_strategy: Literal["ddp", "fsdp", "auto"] = field(
+        default="auto",
+        help="Distributed training strategy to use. If `auto`, will select automatically (no distributed strategy is used when using a single device).",
+        aliases="--ds",
+    )
+
+    micro_batch_size: int = field(default=None, alias="--mb")
+    """If None, use batch_size // num_devices. This is the batch size per device, not the total batch size.
+    You should tune this so that you do not get GPU RAM OOM errors. We automatically calculate the gradient accumulation steps to achieve your desired `batch_size`."""
+
+    eval_micro_batch_size: int = field(default=None)
+    "If None, use micro_batch_size."
+
+    gradient_accumulation_steps: int = field(default=-1)
+    "If -1, set automatically based on batch_size and micro_batch_size."
+
+    precision: Literal["32-true", "16-mixed", "bf16-mixed", "bf16-true", "16-true"] = "bf16-true"
+    compile: bool = field(default=False)
+    "torch.compile model for faster training."
+
+    workers: int = field(default=4, alias="-w")
+    preprocessing_workers: int = field(default=4, aliases="--pw")
+    "Number of workers for preprocessing the datasets. If -1, use all available CPUs."
+
+    data_preprocessing_only: bool = field(default=False)
+    conserve_disk_space: bool = field(default=False)
+
+    ############################
+    ###### Logging & Misc ######
+    ############################
+
+    run_name: str = field(default=None, alias="-n")
+    "Run name for logging."
+
+    seed: int | None = field(default=None)
+
+    only_val: bool = field(default=False)
+    "Only run validation."
+
+    val_before_training: bool = field(default=True)
+    "Run one validation epoch before training."
+
+    out_dir: Path = field(default="out/")
 
     wandb_tags: list[str] = list_field(default=[], alias="-t")
     "Tags for wandb."
@@ -66,35 +140,19 @@ class TrainingArgs:
     debug: bool = field(default=False)
     "If true, wait for debugger to attach at the start of the script."
 
-    devices: int = field(default=1)
-    tpu: bool = False
-    precision: Literal["32-true", "16-true", "bf16-true", "16-mixed", "bf16-mixed"] = "bf16-true"
-    use_anyprecision_adamw: bool = field(default=False)
-    activation_checkpointing: bool = field(default=True)
-    micro_batch_size: int = field(default=4, alias="--mb")
-    eval_micro_batch_size: int = field(default=None)
-    gradient_accumulation_steps: int = field(default=-1)
-    "If -1, set automatically based on batch_size and micro_batch_size."
+    force_deterministic: bool = field(default=False)
+    "Force PyTorch operations to be deterministic. Could be slower."
 
-    fast_model_loading: bool = field(default=True, alias="--fml")
-    "Instantiate model with empty weights. Use if you are loading weights from a checkpoint. Saves a lot of time.",
-
-    compile: bool = field(default=False)
-    "torch.compile model for faster training."
-
-    fsdp_sharding_strategy: Literal["FULL_SHARD", "SHARD_GRAD_OP", "NO_SHARD", "HYBRID_SHARD"] = field(default="SHARD_GRAD_OP")
-    fsdp_limit_all_gathers: bool = field(default=True)
-    fsdp_cpu_offload: bool = field(default=False)
-    smart_cuda_alloc: bool = field(default=False)
-
-    workers: int = field(default=4, alias="-w")
-    preprocessing_workers: int = field(default=4, alias="--pw")
-    data_preprocessing_only: bool = field(default=False)
-    conserve_disk_space: bool = field(default=False)
-    overwrite_data_cache: bool = field(default=False)
+    fast_dev_run: bool = field(default=False)
+    "Do fast run through training and validation with reduced sizes."
 
     def __post_init__(self):
-        self.iter_batch_size = self.micro_batch_size * self.devices
+        if self.micro_batch_size is None:
+            # NOTE: you need to make sure that micro_batch_size can fit into the GPU memory
+            self.micro_batch_size = self.batch_size // self.num_devices
+            assert self.batch_size % self.num_devices == 0
+
+        self.iter_batch_size = self.micro_batch_size * self.num_devices
 
         if self.eval_interval < 1:
             self.eval_interval = int(self.eval_interval * self.training_goal)
@@ -113,37 +171,38 @@ class TrainingArgs:
         if self.gradient_accumulation_steps == -1:
             self.gradient_accumulation_steps = self.batch_size // self.iter_batch_size
         assert self.gradient_accumulation_steps > 0
-        assert self.batch_size == self.micro_batch_size * self.devices * self.gradient_accumulation_steps
+        assert self.batch_size == self.micro_batch_size * self.num_devices * self.gradient_accumulation_steps
 
-        if self.tokenizer is None:
-            self.tokenizer = self.pretrained_checkpoint_dir
-            assert self.pretrained_checkpoint_dir is not None
+        if self.tokenizer_path is None:
+            self.tokenizer_path = self.model_path
+            assert self.model_path is not None
 
         if self.eval_micro_batch_size is None:
             self.eval_micro_batch_size = self.micro_batch_size
 
         # Calculate training constants
         if self.base_unit == "samples":
-            UNITS_PER_ITER = self.iter_batch_size
             UNITS_PER_STEP = self.batch_size
         elif self.base_unit == "tokens":
-            UNITS_PER_ITER = self.iter_batch_size * self.block_size
+            assert self.block_size is not None, "block_size must be set if base_unit is tokens"
             UNITS_PER_STEP = self.batch_size * self.block_size
         elif self.base_unit == "optimizer-steps":
-            UNITS_PER_ITER = 1 / self.gradient_accumulation_steps
             UNITS_PER_STEP = 1
         elif self.base_unit == "iters":
-            UNITS_PER_ITER = 1
             UNITS_PER_STEP = self.gradient_accumulation_steps
         else:
             raise ValueError(f"Unknown training goal unit: {self.base_unit}")
 
-        self.training_goal = int(self.training_goal / UNITS_PER_ITER)
+        self.training_goal = int(self.training_goal / UNITS_PER_STEP)
         self.eval_interval = int(self.eval_interval / UNITS_PER_STEP)
         self.save_interval = int(self.save_interval / UNITS_PER_STEP)
         if self.log_interval == -1:
             self.log_interval = 1
         else:
             self.log_interval = int(self.log_interval / UNITS_PER_STEP)
-        self.warmup_period = int(self.warmup_period / UNITS_PER_ITER)
-        self.lr_decay_period = int(self.lr_decay_period / UNITS_PER_ITER)
+        self.warmup_period = int(self.warmup_period / UNITS_PER_STEP)
+        self.lr_decay_period = int(self.lr_decay_period / UNITS_PER_STEP)
+
+        if self.preprocessing_workers == -1:
+            # Set to all available CPUs, handle SLURM case when only some CPUs are available to the job
+            self.preprocessing_workers = int(os.environ.get("SLURM_JOB_CPUS_PER_NODE", multiprocessing.cpu_count()))
